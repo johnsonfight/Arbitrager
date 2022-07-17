26cbe804ee3f13d170e45e551136c299e14733e7
Johnson Nieh <johnsonfight@gmail.com>
2022-07-18 00:00:37 +0800

    Add getMedianCI() to get median and confidence interval to trigger transaction

--- Arbitrage.js
+++ Arbitrage.js
@@ -1,16 +1,10 @@
 require('dotenv').config();
-const fs = require('fs');
+const {readFileSync} = require('fs');
 const Binance = require('node-binance-api');
-const target = require('./Config.json');
+const config = require('./Config.json');
 const { getPriceBinance } = require('./Binance_trader')
 const { getPrice } = require('./GetPrice_arb1')
 const address = require('./Addresses_arb1_mainnet.json')
-
-function getTime() {
-    var currentdate = new Date().toLocaleString('zh-tw').substr(0, 19);
-    return currentdate
-}
-
 
 const binanceClient = new Binance().options({
     APIKEY: process.env.BINANCE_API_KEY,
@@ -18,19 +12,68 @@ const binanceClient = new Binance().options({
     useServerTime: true,
     family: 0
 });
+
+function getTime() {
+    var currentdate = new Date().toLocaleString('zh-tw').substr(0, 19);
+    return currentdate
+}
+
+function readRefDataFromFile(filename) {
+    const contents = readFileSync(filename, 'utf-8');
+    const arr = contents.split(/\r?\n/);
+  
+    return arr;
+  }
+
+function getMedianCI(arr, diff){
+    console.log(`arr.length = ${arr.length}`)
+    if (arr.length <= 120960){ // 12*60*24*7 (diff data in 7 days)
+        arr.push(diff)
+    }
+    else{
+        arr.shift()
+        arr.push(diff)
+    }
+
+    var arr_sort = arr.sort()
+    var data_map = {
+        "median" : {
+            "index" : Math.round((arr.length+1)/2),
+            "value" : arr_sort[Math.round((arr.length+1)/2)]
+        },
+        "ci_95" : {
+            "index" : Math.round((arr.length+1)*0.95),
+            "value" : arr_sort[Math.round((arr.length+1)*0.95)]
+        },
+        "ci_5" : {
+            "index" : Math.round((arr.length+1)*0.05),
+            "value" : arr_sort[Math.round((arr.length+1)*0.05)]
+        }
+    }
+
+    console.log(data_map)
+
+    return data_map    
+}
+
+var diff_arr = readRefDataFromFile("./sample_diff_data.txt") // Set some init diff data
+var data_map = {}
 async function main() {
     let currentTime = getTime()
+
     try {
-        let priceBinance = await getPriceBinance(binanceClient, target.token0, target.token1)
-        let priceUniswap = await getPrice(address["pool"]["ETHUSDT"], 1)
-        let priceDiff = priceBinance - priceUniswap;
-        console.log(`${currentTime} priceBinance-priceUniswap= ${priceDiff}`)
+        let price_binance = await getPriceBinance(binanceClient, config.token0, config.token1)
+        let price_uniswap = await getPrice(address["pool"]["ETHUSDT"], 1)
+        let price_diff = price_binance - price_uniswap;
+        console.log(`${currentTime} price_binance-price_uniswap= ${price_diff}`)
+
+        data_map = getMedianCI(diff_arr, price_diff)
       }
       catch(err) {
         console.log("Error but nvm~")
       }
 
-    // if (Math.abs(priceDiff) > 3) {
+    // if (Math.abs(price_diff) > 3) {
     //     console.log(`Execute arbitrage.`)
     // }
 }
